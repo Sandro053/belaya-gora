58f61174d8dba8776a7e507c5361c1273a2df3ae
Sandro053 <alex.xolodow@gmail.com>
2025-06-26 00:30:53 +0300

    Add files via upload

---
+++ InfoBlock.js
@@ -0,0 +1,9 @@
+
+export default function InfoBlock() {
+  return (
+    <section style={{ backgroundColor: '#f9f9f9', padding: '1rem', marginTop: '2rem' }}>
+      <h3>Инвестиции в будущее</h3>
+      <p>Развивающаяся локация, растущие цены, инфраструктура и природа — всё в одном месте.</p>
+    </section>
+  );
+}
